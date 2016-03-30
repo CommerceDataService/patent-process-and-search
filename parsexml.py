6bdc4469ccdb422d89b8610c05b4936c67791a05
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-30 16:23:54 -0400

    refactored parts of code to be wrapped in functions instead of inline

--- parsexml.py
+++ parsexml.py
@@ -4,27 +4,38 @@
 
 import json, xmltodict, os
 
-#def alterMetadata(x,image_id):
+#get value for specified tag from a list
+def getData(list,tag):
+    value = list[tag]
 
+    return value
 
+#add data with specified tag to existing list
+def setData(list,tag,text):
+    list[tag] = text
+
+#get specified folder from folder path
+def getFolder(root,level):
+    folder = root.split(os.path.sep)[level]
+
+    return folder
 
 #crawl through each xml file, find corresponding text doc and add to xml node
 for root, dirs, files in os.walk('files/'):
-    for name in files:
+    for name.endswith('.xml') in files:
         if name.endswith('.xml'):
             with open(os.path.join(root,name)) as fd:
                 doc = xmltodict.parse(fd.read())
                 for x in doc['main']['DATA_RECORD']:
-                    image_id = None
-                    if root.split(os.path.sep)[2].startswith('PTAB'):
-                        image_id = x['DOCUMENT_IMAGE_ID']
-                    elif root.split(os.path.sep)[2].startswith('PRPS'):
-                        image_id = x['DOCUMENT_NM']
-                    with open(os.path.join(root+'/PDF_image',image_id+'.txt')) as dr:
-                        print (os.path.join(root+'/PDF_image',image_id+'.txt'))
+                    value = None
+                    pathfolder = getFolder(root,2)
+                    if pathfolder.startswith('PTAB'):
+                        value = getData(x,'DOCUMENT_IMAGE_ID')
+                    elif pathfolder.startswith('PRPS'):
+                        value = getData(x,'DOCUMENT_NM')
+                    with open(os.path.join(root+'/PDF_image',value+'.txt')) as dr:
                         text = dr.read()
-                        x['DOCUMENT_TEXT'] = text
-               
+                        setData(x,'DOCUMENT_TEXT',text)
                 #transform output to json and save to file with same name
                 with open(os.path.join(root,os.path.splitext(name)[0])+'.json','w') as outfile:
                     json.dump(doc,outfile)
