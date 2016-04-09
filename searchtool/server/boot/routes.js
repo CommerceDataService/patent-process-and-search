c8911fc0d2eb49457d1be0fe8cfa6efaabe5ea4a
arowla <arowland@doc.gov>
2016-04-09 00:46:35 -0400

    Extract server config to file
    - Allow required logins to be turned off for development
    - Put Solr URI in config -- don't hardcode

--- searchtool/server/boot/routes.js
+++ searchtool/server/boot/routes.js
@@ -1,12 +1,13 @@
 module.exports = function(app) {
-    var router    =   app.loopback.Router();
-    var humanize  =   require('humanize');
-    var unirest   =   require('unirest');
-    var paginate  =   require('nodejs-yapaginate/lib/main.js');
-    var moment    =   require('moment');
-    var fs = require('fs');
-    var path = require('path');
-    var User = app.models.user;
+    var router   = app.loopback.Router();
+    var humanize = require('humanize');
+    var unirest  = require('unirest');
+    var paginate = require('nodejs-yapaginate/lib/main.js');
+    var moment   = require('moment');
+    var config   = require('../server-config.js');
+    var fs       = require('fs');
+    var path     = require('path');
+    var User     = app.models.user;
 
 
     router.get('/', function(req, res) {
@@ -19,9 +20,7 @@ module.exports = function(app) {
     });
 
 
-    /*
-       Login Post and set session cookie
-       */
+    // Login Post and set session cookie
     router.post('/login', function(req, res) {
         User.login({
             email: req.body.email,
@@ -75,104 +74,108 @@ module.exports = function(app) {
         res.render('help');
     });
 
+    function buildSearch(req, res) {
+        /*
+           example Payload of sessionCookie
+           { ttl: '1209600',
+           id: '2opQLpAHH0ofjkM9TXjnsolxs5LLES00xHbn9sqKU5DSKgpGkBrMor5th7tYf8ep',
+           userId: 1,
+           user:
+           { realm: null,
+           username: null,
+           credentials: null,
+           challenges: null,
+           email: 'darren.culbreath@uspto.gov',
+           emailVerified: null,
+           status: null,
+           created: null,
+           lastUpdated: null,
+           id: 1 } }
+           */
+        // Get From Date
+        var dateRange='';
+        // Only doing this for readiblity. Do not accept blank or undefined dates
+
+        if ((typeof req.query.fromdate !== 'undefined' && req.query.todate !== 'undefined') &&  (req.query.fromdate.length > 2 && req.query.todate.length > 2)) {
+            var fromdate =  moment(req.query.fromdate).format('YYYY-MM-DD');
+            var todate   =  moment(req.query.todate).format('YYYY-MM-DD');
+
+            todate = todate + 'T00:00:00Z';
+            fromdate = fromdate + 'T00:00:00Z';
+
+            dateRange = '%20AND%20doc_date:['+ fromdate+'%20TO%20'+todate+']';
+        }
+
+        // Ensure q var is cast to string
+        var q;
+        if (req.query.q) {
+            q=req.query.q.toString();
+        }
+
+        // Set Pagination to incremnt by 20 results
+        var s = 0;
+        var currentPage = 1;
+        if (req.query.pageno && req.query.pageno > 0) {
+            s = (req.query.pageno -1) *20;
+            currentPage = parseInt(req.query.pageno) ;
+        }
+
+        q = q+dateRange;
+
+        // Build Search .. if no page number set then only show
+        var SEARCH_URL = config.solrURI+'/oafiledatanew/select?q='+q+'&wt=json&indent=true&rows=20&start='+s+'&hl=true&hl.snippets=10&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND&fl=appid,action_type,filename,minread,id,textdata';
+
+        // Debug for logs
+        console.log(SEARCH_URL);
+
+        var body;
+        if (typeof req.query.q !== 'undefined') {
+            unirest.get(SEARCH_URL).end(function (response) {
+                if (typeof response.body !== 'undefined') {
+                    body = JSON.parse(response.body);
+                }
+                if (typeof body.response.docs !== 'undefined') {
+                    res.render('newview', {
+                        result:body.response.docs,
+                        total:humanize.numberFormat(body.response.numFound,0),
+                        pagein:paginate({totalItem:body.response.numFound, itemPerPage:20, currentPage:currentPage, url:'/newsearch',params:{q:q}}),
+                        took:humanize.numberFormat(body.responseHeader.QTime,0 ),
+                        highlighting:body.highlighting,
+                        term:q
+                    });
+                } else {
+                    res.render('newview', {
+                        total:0,
+                        pagein:'',
+                        took:body.responseHeader.QTime,
+                        term:q
+                    });
+                }
+            });
+        } else {
+            res.render('newview');
+        }
+    }
+
     // Main Search
     router.get('/newsearch', function(req, res) {
         var sessionCookie;
+        sessionCookie = JSON.parse(req.cookies.USPTOSession);
+
         if (typeof req.cookies.USPTOSession === 'undefined') {
             res.redirect('/login');
         } else {
             sessionCookie = JSON.parse(req.cookies.USPTOSession);
         }
-        if (sessionCookie && typeof sessionCookie.id !== 'undefined') {
 
-            /*
-               example Payload of sessionCookie
-               { ttl: '1209600',
-               id: '2opQLpAHH0ofjkM9TXjnsolxs5LLES00xHbn9sqKU5DSKgpGkBrMor5th7tYf8ep',
-               userId: 1,
-               user:
-               { realm: null,
-               username: null,
-               credentials: null,
-               challenges: null,
-               email: 'darren.culbreath@uspto.gov',
-               emailVerified: null,
-               status: null,
-               created: null,
-               lastUpdated: null,
-               id: 1 } }
-               */
-            // Get From Date
-            var dateRange='';
-            // Only doing this for readiblity. Do not accept blank or undefined dates
-
-            if ((typeof req.query.fromdate !== 'undefined' && req.query.todate !== 'undefined') &&  (req.query.fromdate.length > 2 && req.query.todate.length > 2)) {
-                var fromdate =  moment(req.query.fromdate).format('YYYY-MM-DD');
-                var todate   =  moment(req.query.todate).format('YYYY-MM-DD');
-
-                todate = todate + 'T00:00:00Z';
-                fromdate = fromdate + 'T00:00:00Z';
-
-                dateRange = '%20AND%20doc_date:['+ fromdate+'%20TO%20'+todate+']';
-            }
-
-            // Ensure q var is cast to string
-            var q;
-            if (req.query.q) {
-                q=req.query.q.toString();
-            }
-
-            // Set Pagination to incremnt by 20 results
-            var currentPage;
-            var s;
-            if (req.query.pageno && req.query.pageno > 0) {
-                s = (req.query.pageno -1) *20;
-                currentPage = parseInt(req.query.pageno) ;
-            }
-
-            q = q+dateRange;
-
-            // Build Search .. if no page number set then only show
-            var SEARCH_URL;
-            if (!req.query.pageno) {
-                SEARCH_URL= 'http://54.208.116.77:8983/solr/oafiledatanew/select?q='+q+'&wt=json&indent=true&rows=20&start=0&hl=true&hl.snippets=10&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND&fl=appid,action_type,filename,minread,id,textdata';
-                currentPage = 1;
+        if (config.requireLogin) {
+            if (sessionCookie && typeof sessionCookie.id !== 'undefined') {
+                buildSearch(req, res);
             } else {
-                SEARCH_URL= 'http://54.208.116.77:8983/solr/oafiledatanew/select?q='+q+'&wt=json&indent=true&rows=20&start='+s+'&hl=true&hl.snippets=10&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND&fl=appid,action_type,filename,minread,id,textdata';
-            }
-
-            // Debug for logs
-            console.log(SEARCH_URL);
-
-            var body;
-            if (typeof req.query.q !== 'undefined') {
-                unirest.get(SEARCH_URL).end(function (response) {
-                    if (typeof response.body !== 'undefined') {
-                        body = JSON.parse(response.body);
-                    }
-                    if (typeof body.response.docs !== 'undefined') {
-                        res.render('newview', {
-                            result:body.response.docs,
-                            total:humanize.numberFormat(body.response.numFound,0),
-                            pagein:paginate({totalItem:body.response.numFound, itemPerPage:20, currentPage:currentPage, url:'/newsearch',params:{q:q}}),
-                            took:humanize.numberFormat(body.responseHeader.QTime,0 ),
-                            highlighting:body.highlighting,
-                            term:q
-                        });
-                    } else {
-                        res.render('newview', {
-                            total:0,
-                            pagein:'',
-                            took:body.responseHeader.QTime,
-                            term:q
-                        });
-                    }
-                });
-            } else {
-                res.render('newview');
+                res.redirect('/login');
             }
         } else {
-            res.redirect('/login');
+            buildSearch(req, res);
         }
     });
 
@@ -186,20 +189,13 @@ module.exports = function(app) {
             q=req.query.q.toString();
         }
 
-        var currentPage;
-        var s;
+        var s = 0;
+        var currentPage = 1;
         if (req.query.pageno && req.query.pageno > 0) {
             s = (req.query.pageno -1) *20;
             currentPage = parseInt(req.query.pageno) ;
         }
-
-        var SEARCH_URL;
-        if (!req.query.pageno) {
-            SEARCH_URL= 'http://54.208.116.77:8983/solr/oafiledatanew/select?q='+q+'&wt=csv&indent=falue&rows=100&start=0&hl=false&hl.snippets=5&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND&fl=appid,action_type,filename,minread,id,textdata';
-            currentPage = 1;
-        } else {
-            SEARCH_URL= 'http://54.208.116.77:8983/solr/oafiledatanew/select?q='+q+'&wt=csv&indent=true&rows=20&start='+s+'&hl=true&hl.snippets=5&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND&fl=appid,action_type,filename,minread,id,textdata';
-        }
+        var SEARCH_URL= config.solrURI+'/oafiledatanew/select?q='+q+'&wt=csv&indent=falue&rows=100&start='+s+'&hl=false&hl.snippets=5&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND&fl=appid,action_type,filename,minread,id,textdata';
 
         // Create the filename for the CSV and remove any special characters
         var csvfilename = q;
