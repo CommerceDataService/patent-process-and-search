f39aee026d05c4a6aa3a8eace106abbcc456d00e
Mark Brown II <mbrownii@doc.gov>
2016-07-07 13:09:02 -0400

    Changed solURL to make search work

--- searchtool/server/server-config.js
+++ searchtool/server/server-config.js
@@ -1,6 +1,6 @@
 var config = {
     'requireLogin': false,
-    'solrURI': 'http://52.90.109.169:8983/solr`'
+    'solrURI': 'http://ec2-52-91-115-157.compute-1.amazonaws.com:8983/solr'
 };
 
 module.exports = config;
