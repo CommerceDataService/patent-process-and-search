61421bdaf365d02c28e6187b33d5d09032eddfc3
noonkay <negar.kalbasi@gmail.com>
2016-07-08 14:02:52 -0400

    Retaining value of date widget. Cleaning up search box. Date refining will not concatenate

--- searchtool/server/boot/search.js
+++ searchtool/server/boot/search.js
@@ -24,15 +24,14 @@ exports.buildSearch = function (req, res) {
     // Get From Date
     var dateRange='';
     // Only doing this for readiblity. Do not accept blank or undefined dates
-
     if ((typeof req.query.fromdate !== 'undefined' && req.query.todate !== 'undefined') &&  (req.query.fromdate.length > 2 && req.query.todate.length > 2)) {
         var fromDate = +new Date(req.query.fromdate)/1000;
         var toDate   = +new Date(req.query.todate)/1000;
-        dateRange = '%20AND%20doc_date:['+ fromDate+'%20TO%20'+toDate+']';
+        dateRange = 'doc_date:['+ fromDate+'%20TO%20'+toDate+']';
     }
 
     // Ensure q var is cast to string
-    var q;
+    var q, fq;
     if (req.query.q) {
         q=req.query.q.toString();
     }
@@ -44,9 +43,10 @@ exports.buildSearch = function (req, res) {
         s = (req.query.pageno -1) *20;
         currentPage = parseInt(req.query.pageno) ;
     }
-    q = q+dateRange;
+    fq = "&fq=type:" + req.query.dataset;
+    fq += "&fq=" + dateRange;
     // Build Search .. if no page number set then only show
-    var SEARCH_URL = config.solrURI+'/select?q={!q.op=AND df=textdata}'+q+'&fq=type:'+req.query.dataset+'&wt=json&indent=true&rows=20&start='+s+'&hl=true&hl.snippets=10&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND';
+    var SEARCH_URL = config.solrURI+'/select?q={!q.op=AND df=textdata}'+q+fq+'&wt=json&indent=true&rows=20&start='+s+'&hl=true&hl.snippets=10&hl.fl=textdata&hl.fragsize=200&hl.simple.pre=<code>&hl.simple.post=</code>&hl.usePhraseHighlighter=true&q.op=AND';
     if (req.query.dataset == 'oa'){
        SEARCH_URL += '&fl=appid,action_type,filename,minread,id,textdata';
     }else if (req.query.dataset == 'ptab'){
@@ -73,7 +73,9 @@ exports.buildSearch = function (req, res) {
                     term:q,
                     ptab: ptab,
                     email: req.body.email,
-                    accessOK: !!(! config.requireLogin || token.id)
+                    accessOK: !!(! config.requireLogin || token.id),
+                    todate: req.query.todate,
+                    fromdate:req.query.fromdate
                 });
             } else {
                 res.render('newview', {
