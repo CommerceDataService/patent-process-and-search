3046a8328ac2dc523b5bc30f78066456e1f5e8fd
Mark Brown II <mbrownii@doc.gov>
2016-07-12 09:17:45 -0400

    Persist Art Unit number across pagination

--- searchtool/server/boot/search.js
+++ searchtool/server/boot/search.js
@@ -76,7 +76,7 @@ exports.buildSearch = function (req, res) {
                 res.render('newview', {
                     result:body.response.docs,
                     total:humanize.numberFormat(body.response.numFound,0),
-                    pagein:paginate({totalItem:body.response.numFound, itemPerPage:20, currentPage:currentPage, url:'/newsearch',params: {q: q, dataset: req.query.dataset} }),
+                    pagein:paginate({totalItem:body.response.numFound, itemPerPage:20, currentPage:currentPage, url:'/newsearch',params: {q: q, dataset: req.query.dataset, fromdate: req.query.fromdate, todate: req.query.todate, dn_dw_dn_gau_cd: req.query.dn_dw_dn_gau_cd} }),
                     took:humanize.numberFormat(body.responseHeader.QTime,0 ),
                     highlighting:body.highlighting,
                     term:q,
